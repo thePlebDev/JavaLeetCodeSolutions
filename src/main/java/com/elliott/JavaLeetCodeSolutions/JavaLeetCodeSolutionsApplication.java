2484759c1ecdc2943b889a8698daf4d4cdf87575
Tristan Elliott <>
2022-07-30 12:30:42 -0300

    initial commit

---
+++ src/main/java/com/elliott/JavaLeetCodeSolutions/JavaLeetCodeSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.elliott.JavaLeetCodeSolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLeetCodeSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLeetCodeSolutionsApplication.class, args);
+	}
+
+}

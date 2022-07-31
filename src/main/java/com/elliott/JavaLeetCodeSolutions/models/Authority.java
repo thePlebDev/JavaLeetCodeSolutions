d9b41dc64dc0f99d12c3c2828db3bb9715bda1a2
Tristan Elliott <>
2022-07-31 20:36:22 -0300

    adding basic user auth schema design

---
+++ src/main/java/com/elliott/JavaLeetCodeSolutions/models/Authority.java
@@ -0,0 +1,38 @@
+package com.elliott.JavaLeetCodeSolutions.models;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Authority extends AbstractEntity{
+
+    private String name;
+
+    @JoinColumn(name = "user")
+    @ManyToOne
+    private User user;
+    public Authority(){}
+    public Authority(String name, User user){
+        this.name = name;
+        this.user = user;
+    }
+
+    //GETTERS
+    public String getName(){
+        return this.name;
+    }
+    public User getUser(){
+        return this.user;
+    }
+
+    //SETTERS
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setUser(User user){
+        this.user = user;
+    }
+
+
+}
